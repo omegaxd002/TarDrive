2695f9d87cebd352b460cfba8a5db217087b3017
omegaxd002 <84318618+omegaxd002@users.noreply.github.com>
2021-05-21 16:53:31 +0300

    Update .constants.js

--- src/.constants.js
+++ src/.constants.js
@@ -1,19 +1,19 @@
 module.exports = Object.freeze({
-  TOKEN: 'bot_token',
+  TOKEN: '1589480923:AAEPa48lKm6qgvjVEhQi5TjA21QGgEnWBPE',
   ARIA_SECRET: 'viking',
   ARIA_DOWNLOAD_LOCATION: '/storage/emulated/0/Android/data/data/com.termux/files/home/TarDriveBot/downloads (no trailing "/")',
   ARIA_DOWNLOAD_LOCATION_ROOT: '/', //The mountpoint that contains ARIA_DOWNLOAD_LOCATION
   ARIA_FILTERED_DOMAINS: [], // Prevent downloading from URLs containing these substrings
   ARIA_FILTERED_FILENAMES: [], // Files/top level directories with these substrings in the filename won't be downloaded
   ARIA_PORT: 8210, // Port for aria2c RPC server, if you change this here, make sure to update aria.sh as well
-  GDRIVE_PARENT_DIR_ID: '1tPGmz4eHXLEWYczECi6G9RbQATzY_C9n',
+  GDRIVE_PARENT_DIR_ID: '1WMMFE22vkjCUXk48iVno0IFpJYTrXAxy',
   OTHER_GDRIVE_DIR_IDS: [], // This is needed if u want to look for files in multiple dirs on list command
-  SUDO_USERS: [1597196024, 1095264362],	// Telegram user IDs. These users can use the bot in any chat.
-  AUTHORIZED_CHATS: [-1001451622377, -1001321363429],	// Telegram chat IDs. Anyone in these chats can use the bot.
+  SUDO_USERS: [952345950],	// Telegram user IDs. These users can use the bot in any chat.
+  AUTHORIZED_CHATS: [-1001451096196],	// Telegram chat IDs. Anyone in these chats can use the bot.
   STATUS_UPDATE_INTERVAL_MS: 12000, // A smaller number will update faster, but might cause rate limiting
   DRIVE_FILE_PRIVATE: {
-    ENABLED: false,
-    EMAILS: ['mrnu.2003.1@gmail.com']
+    ENABLED: true,
+    EMAILS: ['jskrappa123@gmail.com']
   },
   DOWNLOAD_NOTIFY_TARGET: {  // Information about the web service to notify on download completion.
     enabled: false,   // Set this to true to use the notify functionality
@@ -22,12 +22,12 @@ module.exports = Object.freeze({
     path: '/botNotify'
   },
   COMMANDS_USE_BOT_NAME: {
-    ENABLED: true,  // If true, all commands except '/list' has to have the bot username after the command
+    ENABLED: false,  // If true, all commands except '/list' has to have the bot username after the command
     NAME: "@TarGDrive1Bot"
   },
   IS_TEAM_DRIVE: true,
   USE_SERVICE_ACCOUNT: false,
-  INDEX_DOMAIN: "https://td.thyviking.workers.dev/0:/TarDriveBot/",
+  INDEX_DOMAIN: "https://basalibas.basalibas.workers.dev/0:/",
   TELEGRAPH_TOKEN: 'ee2862d6df4637cabfee0134b03f9bc3dabfdec08444e169e8e25c5d1dbf', // Telegraph token, if you want to show search results in telegra.ph else keep it blank,
   STOP_DUPLICATE_MIRRORS: true // To stop duplicate mirror
 });
